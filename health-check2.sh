7002abee5fa421df9d186de555425a8f9a138e1c
Miguel Beltrán <miguel.socialmedia@outlook.com>
2021-02-11 09:19:34 -0800

    Update health-check2.sh

--- health-check2.sh
+++ health-check2.sh
@@ -47,19 +47,17 @@ healthcheck() {
     echo $result
 }
 
-while [[ true ]]; do
-   result=`healthcheck $endpoint` 
-   declare status
-   if [[ -z $result ]]; then 
-      status="N/A"
-   else
-      status=${result:9:3}
-   fi 
-   timestamp=$(date "+%Y%m%d-%H%M%S")
-   if [[ -z $hasUrl ]]; then
-     echo "$timestamp | $status "
-   else
-     echo "$timestamp | $status | $endpoint " 
-   fi 
-   sleep $duration
-done
+result=`healthcheck $endpoint` 
+declare status
+if [[ -z $result ]]; then 
+  status="N/A"
+else
+  status=${result:9:3}
+fi 
+timestamp=$(date "+%Y%m%d-%H%M%S")
+if [[ -z $hasUrl ]]; then
+ echo "$timestamp | $status "
+else
+ echo "$timestamp | $status | $endpoint " 
+fi 
+sleep $duration
